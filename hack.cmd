f55d52742d393eb2e20d1cafb31cd0657e3181ce
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2019-11-14 01:52:47 -0600

    Add files via upload

---
+++ hack.cmd
@@ -0,0 +1,44 @@
+@echo off
+:welcome
+color 02
+echo.W
+timeout 0 >nul
+cls
+echo.WE
+timeout 0 >nul
+cls
+echo.WEL
+timeout 0 >nul
+cls
+echo.WELC
+timeout 0 >nul
+cls
+echo.WELCO
+timeout 0 >nul
+cls
+echo.WELCOM
+timeout 0 >nul
+cls
+echo.WELCOME
+timeout 0 >nul
+cls
+echo.WELCOME B
+timeout 0 >nul
+cls
+echo.WELCOME BA
+timeout 0 >nul
+cls
+echo.WELCOME BAC
+timeout 0 >nul
+cls
+echo.WELCOME BACK
+timeout 3 >nul
+cls
+color 04
+goto:hack
+
+
+
+:hack
+echo. %RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%
+goto:hack
